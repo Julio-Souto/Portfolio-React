917a33ec238044ff35cbfb83f85f3d58b8e1bbf5
Julio-Souto <julio.souto.zas@gmail.com>
2023-07-05 11:58:06 +0200

    Margin bottom

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,6 +1,6 @@
 export default function LoadingSpinner() {
   return (
-    <div className="flex justify-center spinner-container">
+    <div className="flex justify-center mb-6 spinner-container">
       <div className="loading-spinner">
       </div>
     </div>
